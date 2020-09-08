3ef2682c682c105d89a383883efacbf9daceafe1
Feyd-Rauth <atreides.pasha@yandex.ru>
2020-09-08 14:42:59 +0300

    First task. Every new process has it own unique PID, therefore PID's changing even though it is the same program

---
+++ ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+
+int main()
+{
+    int n = 5;
+    if(fork() == 0){
+        printf("Hello from child %d\n", getpid() - n);
+    } else {
+        printf("Hello from parent %d\n", getpid() - n);
+    }
+    return 0;
+}
